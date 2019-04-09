2a512ccacf737a97e00150a842abc6dc0fb8a319
Olga R <rolya85@bk.ru>
2019-04-09 13:00:42 +0300

    Player"

---
+++ AudioPlayer/AudioPlayer/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayer
+{
+    class Player
+    {
+        private const int _maxVolume = 100;
+        private int _volume;
+        public int Volume
+        {
+            get
+            {
+                return _volume;
+            }
+            private set
+            {
+                if(value > _maxVolume)
+                {
+                    _volume = _maxVolume;
+                }
+                else if(value < 0)
+                {
+                    _volume = 0;
+                }
+                else
+                {
+                    _volume = value;
+                }
+            }
+        }
+        bool IsLock;
+        public Song[] Songs;
+
+        public void Play()
+        {
+            for (int i = 0; i < Songs.Length; i++)
+            {
+                Console.WriteLine(Songs[i].Title);
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+
+        public void VolumeUp()
+        {
+            Volume += 5;
+            Console.WriteLine("Volume is: " + Volume);
+        }
+
+        public void VolumeDown()
+        {
+            Volume -= 5;
+            Console.WriteLine("Volume is: " + Volume);
+        }
+    }
+}
---
+++ AudioPlayer/AudioPlayer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AudioPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var song1 = new Song();
+            song1.Title = "Дым сигарет с ментолом";
+            song1.Duration = 300;
+            song1.Artist = new Artist
+            {
+                Name = "Нэнси"
+            };
+            var song2 = new Song();
+            song2.Title = "Anaconda";
+            song2.Duration = 270;
+            song2.Artist = new Artist
+            {
+                Name = "Nicki Minaj"
+            };
+            var player = new Player();
+            player.Songs = new[] { song1, song2 };
+
+            while (true)
+            {
+                switch (ReadLine())
+                {
+                    case "Up":
+                        {
+                            player.VolumeUp();
+                        }break;
+
+                    case "Down":
+                        {
+                            player.VolumeDown();
+                        }break;
+
+                    case "P":
+                        {
+                            player.Play();
+                        }break;
+                }
+            }
+        }
+    }
+}
