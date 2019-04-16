4c29e4c87faa8f2bbfd66082838a7d8874ad4ced
Olga R <rolya85@bk.ru>
2019-04-16 16:07:55 +0300

    b6"

---
+++ B-6-Arrays/B-6-Arrays-Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.ReadLine();
+        }
+
+        public static void Pyatnashki()
+        {
+           
+
+        }
+
+        public static void PoemExample()
+        {
+            
+        }
+    }
+}
