9bc6d2a543ac69e5bdc39397211e140e84a7c1d8
Olga R <rolya85@bk.ru>
2019-04-17 18:30:47 +0300

    L7

---
+++ B-7(6)-Collections/B-7-Collection/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayListExample();
+
+            Console.ReadLine();
+        }
+
+
+
+        public static void ArrayListExample()
+        {
+            var poem = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                var song = new Song();
+                song.Lyrics = Console.ReadLine();
+                poem.Add(song);
+            }
+
+            //poem.Sort();
+            poem.RemoveAt(poem.Count - 1);
+
+            object[] myArray = poem.ToArray();
+
+            foreach (var item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public class Song
+        {
+            public string Lyrics;
+
+            public override string ToString()
+            {
+                return this.Lyrics;
+            }
+        }
+    }
+}
