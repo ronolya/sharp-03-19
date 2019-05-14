57c580748c1b0de0bd03d23fc56c1832f96958ea
Olga R <rolya85@bk.ru>
2019-05-14 12:32:46 +0300

    no message

--- A-5(4)-Garbage-Collection/A-4-Garbage-Collection/Practice.cs
+++ A-5(4)-Garbage-Collection/A-4-Garbage-Collection/Practice.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Advenced.Lesson_4
 {
     public partial class Practice
@@ -15,6 +16,7 @@ namespace Advenced.Lesson_4
         /// </summary>
         public static void AL4_P1_P2_P3_5_InstanceCounter()
         {
+
         }
 
         /// <summary>
@@ -23,6 +25,44 @@ namespace Advenced.Lesson_4
         /// </summary>
         public static void AL4_P4_P5_5_InstanceCounter()
         {
+          //  User user;
+            for (int i = 0; i < 50000000; i++)
+            {
+                var user = new User();
+                
+                    if (i % 50000 == 0)
+                    {
+                        //Console.WriteLine( $"memory before  - {GC.GetTotalMemory(false)}");
+                        Console.WriteLine(User.counter);
+                    }
+              
+                
+                
+
+            }
+            Console.WriteLine(User.counter);
         }
     }
+    public class User: IDisposable
+        {
+        public static int counter =  0 ;
+        bool isDisposed { get; set; }
+        public void Dispose()
+        {
+            isDisposed = true;
+            counter--;
+            GC.SuppressFinalize(this);
+        }
+        public User()
+        {
+            counter++;
+        }
+        ~User()
+        {
+            //counter--;
+            if (isDisposed == false) { Dispose(); }
+            
+        }
+    }
+
 }
--- A-5(4)-Garbage-Collection/A-4-Garbage-Collection/Program.cs
+++ A-5(4)-Garbage-Collection/A-4-Garbage-Collection/Program.cs
@@ -6,7 +6,7 @@ namespace Advenced.Lesson_4
     {        
         public static void Main(string[] args)
         {
-
+            Practice.AL4_P4_P5_5_InstanceCounter();
             Console.ReadLine();
         }
     }
